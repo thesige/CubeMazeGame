dcccf1a2e15e85006822a633979358e2c7c9be54
jermann <github@jermann.cc>
2018-03-22 20:21:33 +0100

    v2.2
    Map size choosing is now dynamically to the users display size

--- Projekt Prototyp1/src/gui/Hub.java
+++ Projekt Prototyp1/src/gui/Hub.java
@@ -1,30 +1,43 @@
 package gui;
+import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
 import java.awt.event.*;
 import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class Hub extends JFrame implements ActionListener {
-
-	private JButton start[] = new JButton[6];
+	private JButton start[];
+	private int maxFieldSize;
 	
 	public Hub() {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int width = (int) screenSize.getWidth();
+		int height = (int) screenSize.getHeight();
+		int screen;
+		if(height < width) {
+			screen = (int)height;
+		}else {
+			screen = (int)width;
+		}
+		maxFieldSize = (screen / 300) - 2;
+		start = new JButton[maxFieldSize];
+		
 		this.setLayout(new GridLayout(2, 3));
-		this.setSize(500, 500);
-		this.setTitle("Programm");
+		this.setSize(width / 2, height / 2);
+		this.setTitle("Maze");
 		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
-		for(int i = 0; i < 6; i++) {
+		for(int i = 0; i < maxFieldSize; i++) {
 			start[i] = new JButton(String.valueOf(i + 2));
 			this.add(start[i]);
 			start[i].addActionListener(this);
 		}
-		
 		this.setVisible(true);
 	}
 
 	public void actionPerformed (ActionEvent e){
-		for(int i = 0; i < 6; i++) {
+		for(int i = 0; i < maxFieldSize; i++) {
 			if (e.getSource() == start[i]){
 				this.dispose();
 				@SuppressWarnings("unused")
